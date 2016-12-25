1e7851705d3e2bcb8a3032d90d452377f0f8fcfb
baldekarim <abdkarim.balde@gmail.com>
2016-12-25 23:21:29 +0100

    Initial Commit

---
+++ AgeOfWar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AgeOfWar
+//
+//  Created by Abdoul Karim BALDÉ on 25/12/2016.
+//  Copyright © 2016 Abdoul Karim BALDÉ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
