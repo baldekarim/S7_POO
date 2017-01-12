d7810d6979a0ba9370384cc8da9669ed0eca21bc
Karim <abdoul-karim.balde@u-psud.fr>
2017-01-12 21:25:26 +0100

    Changement Conception

--- AgeOfWar/Unite.hpp
+++ AgeOfWar/Unite.hpp
@@ -2,37 +2,55 @@
 #ifndef Unite_hpp
 #define Unite_hpp
 #include <iostream>
-#include "Case.hpp"
+#include <string>
+#include <map>
 
 class Unite
 {
-	private :
+	
+  private :
 	
 	int prix;
 	int ptsVie;
 	int ptsAttaque;
 	int portee;
 	int numeroCase;
-	Case *saCase;
+	char sensDeplacement;
+	static bool init;
 	
-    public:
+  public:
+	
+	typedef std::map<int, Unite*> mapT;
+	static mapT plateau;
 	
 	Unite() {}
-	Unite(int p, int v, int a, int por) : prix(p), ptsVie(v), ptsAttaque(a), portee(por) {}
-	virtual ~Unite() {}
-	//bool estEgal(Unite const& u)const;
+	Unite(int p, int v, int a, int por);
+	
+	virtual ~Unite() {};
+	
+	virtual std::string getClasse() const = 0;
+	
+	void setNumeroCase(int n, char sens) {
+		numeroCase = n;
+		sensDeplacement = sens;
+	}
+	
 	void avancer();
-	Case getCase() { return *saCase; }
+	
 	virtual void attaquer() = 0;
-	void action1();
+	
+	bool action1();
+	
 	void action2();
-	void action3();
+	
+	virtual void action3();
+	
+	static Unite* getUnite(int ind);
+	
+	static void setUnite(int ind, Unite *u);
+	
+	static mapT creerMap();
 	
 };
 
-/*bool operator==(Unite const& u1, Unite const& u2) {
-	return u1.estEgal(u2);
-}*/
-
 #endif
-// surcharger operateur== (suggestion ci-dessus), définir méthode getCase qui renvoie la case de l'unité
